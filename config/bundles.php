ac0c820a59e4f105c8b5cd46d69f39c4e00b2764
Joshua <joshua.naine@hotmail.com>
2022-07-07 17:34:07 +0200

    first commit

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
